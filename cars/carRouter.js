a80459b930c18e82079ed035d03fad8ae8c06343
Ben Haus <ben@little-haus.com>
2020-05-12 15:44:51 -0400

    car-dealer.db3 created

---
+++ cars/carRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const knex = require('knex');
+
+const db = require('../data/dbConnection.js');
+
+
+const router = express.Router();
+
+
+module.exports = router;
